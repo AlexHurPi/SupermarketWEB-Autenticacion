05c01e6fd17a3cab0598bdfd6e75383f3d80c60e
AlexHurPi <Alexander.hurtado7170@gmail.com>
2023-05-07 12:53:20 -0500

    Add Dbo Provider

---
+++ Migrations/20230507174045_SegundaMigracion.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SupermarketWEB.Migrations
+{
+    /// <inheritdoc />
+    public partial class SegundaMigracion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+			migrationBuilder.CreateTable(
+			   name: "Provider",
+			   columns: table => new
+			   {
+				   Id = table.Column<int>(type: "int", nullable: false)
+					   .Annotation("SqlServer:Identity", "1, 1"),
+				   Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+				   Description = table.Column<string>(type: "nvarchar(max)", nullable: true)
+			   },
+			   constraints: table =>
+			   {
+				   table.PrimaryKey("PK_Provider", x => x.Id);
+			   });
+
+		}
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+			//DropTable("dbo.Provider");
+			migrationBuilder.DropTable(
+			   name: "Provider");
+		}
+    }
+}
