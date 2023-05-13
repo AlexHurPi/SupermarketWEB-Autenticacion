a8dd7a5d6929056d1864adf57a29b5627f263c42
AlexHurPi <Alexander.hurtado7170@gmail.com>
2023-05-13 18:34:44 -0500

    Setting to edit sales file

--- dbo.Sales.sql
+++ dbo.Sales.sql
@@ -1,11 +1,11 @@
 ﻿CREATE TABLE [dbo].[Sales] (
     [Id]           INT             IDENTITY (1, 1) NOT NULL,
-    [Date]         NVARCHAR (MAX)  NOT NULL,
+    [Date]         NVARCHAR (MAX)  NULL,
     [CustomerId]   NVARCHAR (MAX)  NULL,
     [ProductName]  NVARCHAR (MAX)  NULL,
-    [Quantity]     INT             NOT NULL,
-    [ProductPrice] NVARCHAR (MAX)  NULL,
-    [TotalSale]    DECIMAL (15, 2) NOT NULL,
+    [Quantity]     INT             NULL,
+    [ProductPrice] DECIMAL (15, 2)  NULL,
+    [TotalSale]    DECIMAL (15, 2) NULL,
     [PayModeName]  NVARCHAR (MAX)  NULL,
     [Observation]  NVARCHAR (MAX)  NULL,
     CONSTRAINT [PK_Sales] PRIMARY KEY CLUSTERED ([Id] ASC)
