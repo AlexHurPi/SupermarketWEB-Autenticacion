5547851aed895a585f0e984508effdae3d505631
AlexHurPi <Alexander.hurtado7170@gmail.com>
2023-05-13 19:38:34 -0500

    Purchase index

--- Pages/Purchases/Index.cshtml.cs
+++ Pages/Purchases/Index.cshtml.cs
@@ -1,12 +1,31 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SupermarketWEB.Data;
+using SupermarketWEB.Models;
 
 namespace SupermarketWEB.Pages.Purchases
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
-        {
-        }
-    }
+		private readonly SupermarketContext _context;
+
+		public IndexModel(SupermarketContext context)
+		{
+			_context = context;
+		}
+
+		public IList<Purchase?> Purchases { get; set; } = default!;
+
+		public async Task OnGetAsync()
+		{
+			if (_context.Purchases != null)
+			{
+				Purchases = await _context.Purchases.ToListAsync();
+			}
+
+		}
+	}
 }
+    
+
