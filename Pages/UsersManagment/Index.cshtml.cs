db73fabc8a96deb19fe255ac5755cbfa47c2dfa2
AlexHurPi <Alexander.hurtado7170@gmail.com>
2023-05-20 16:48:02 -0500

    Index, Create and Delete

---
+++ Pages/UsersManagment/Index.cshtml.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SupermarketWEB.Data;
+using SupermarketWEB.Models;
+
+namespace SupermarketWEB.Pages.UsersManagment
+{
+    public class IndexModel : PageModel
+    {
+		private readonly SupermarketContext _context;
+
+		public IndexModel(SupermarketContext context)
+		{
+			_context = context;
+		}
+
+		public IList<User> User { get; set; } = default!;
+
+		public async Task OnGetAsync()
+		{
+			if (_context.Users != null)
+			{
+				User = await _context.Users.ToListAsync();
+			}
+		}
+	}
+}
