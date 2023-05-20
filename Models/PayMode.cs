8918bbb89311de8efc5a266389729f72e0957ebb
AlexHurPi <Alexander.hurtado7170@gmail.com>
2023-05-20 17:40:35 -0500

    Authorize to Customer, PayMode, Product, Provider and User

--- Models/PayMode.cs
+++ Models/PayMode.cs
@@ -1,8 +1,11 @@
-﻿namespace SupermarketWEB.Models
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace SupermarketWEB.Models
 {
 	public class PayMode
 	{
-		public int Id { get; set; }
+        [Authorize]
+        public int Id { get; set; }
 		public string Name { get; set; }
 		public string Description { get; set; } 
 	}
--- Models/User.cs
+++ Models/User.cs
@@ -1,7 +1,9 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Authorization;
+using System.ComponentModel.DataAnnotations;
 
 namespace SupermarketWEB.Models
 {
+    [Authorize]
     public class User
     {
         public int Id { get; set; }
