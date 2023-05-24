3a6cd97d00d63b331a1c70476a9570bf3cc977f7
AlexHurPi <Alexander.hurtado7170@gmail.com>
2023-05-24 18:45:35 -0500

    Acces to admin

--- Pages/Account/Login.cshtml.cs
+++ Pages/Account/Login.cshtml.cs
@@ -32,7 +32,7 @@ namespace SupermarketWEB.Pages.Account
             {
                 var email= user.Email;
                 var password= user.Password;
-                if (User.Email == email && User.Password == password)
+                if (User.Email == email && User.Password == password || User.Email=="profe@gmail.com" && User.Password=="12345")
                 {
                     //se crea los claim, datos a almacenar en la cookie
                     var claims = new List<Claim>
